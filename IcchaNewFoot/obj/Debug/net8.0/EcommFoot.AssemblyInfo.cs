9f2e21f888697cdfa8311161629cd2c72866f35f
ichcha595 <ichchavasunde595@gmail.com>
2024-09-16 10:41:49 +0530

    icchapush

--- IcchaNewFoot/obj/Debug/net8.0/EcommFoot.AssemblyInfo.cs
+++ IcchaNewFoot/obj/Debug/net8.0/EcommFoot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommFoot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1eb071278ea6964c0abea7b0ade2f37d03396ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d77fda25a128bc0fafc5892c7c79d4cf76b63fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommFoot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommFoot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
